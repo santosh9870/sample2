eeed6b838990a5f9b918211d123827aa459c79cd
TLead@gmail.com <SANTOSH@192.168.1.79>
2023-08-22 09:20:55 +0530

    second

---
+++ proj2/src/package1/Proj2.java
@@ -0,0 +1,14 @@
+package package1;
+
+public class Proj2 {
+
+	public Proj2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
